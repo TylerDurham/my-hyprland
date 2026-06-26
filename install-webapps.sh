d1d0701c4982752f56b8d3af1bab985593712569
Tyler Durham <TylerDurham@noreply.users.github.com>
2026-06-25 21:16:49 -0500

    feat(webapps): add LitCharts webapp

--- install-webapps.sh
+++ install-webapps.sh
@@ -5,3 +5,4 @@ hypr-install-webapp "Logos" "https://app.logos.com" "https://github.com/TylerDur
 hypr-install-webapp "Gitea" "https://gitea.snork.co" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/gitea.svg"
 hypr-install-webapp "Github" "https://github.com/TylerDurham" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/github-light.svg"
 hypr-install-webapp "Claude.ai" "https://claude.ai/new" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/claude-ai.svg"
+hypr-install-webapp "LitCharts" "https://www.litcharts.com/" "https://cdn.jsdelivr.net/gh/selfhst/icons/svg/calibre.svg"
