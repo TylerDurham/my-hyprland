0e1994ad2f5cd0b0643491d38d880da60bd4a3ff
Tyler Durham <TylerDurham@noreply.users.github.com>
2026-07-11 17:15:48 -0500

    feat(nmtui): add themed nmtui colors and env vars for lovely-day theme
    - Add NEWT_COLORS_FILE env var pointing to current theme colors
    - Add nmtui.colors palette for lovely-day (Ayu Dark) in both config and theme dirs
    - Sort envs.lua entries alphabetically
    - Add blueman-manager wrapped class to modal-utility-window rule

--- .config/hypr/modules/applications/system.lua
+++ .config/hypr/modules/applications/system.lua
@@ -2,7 +2,7 @@
 hl.window_rule({
   name = "modal-utility-window",
   match = {
-    class = "^(blueman-manager|nm-connection-editor|org.pulseaudio.pavucontrol)$"
+    class = "^(.blueman-manager-wrapped|blueman-manager|nm-connection-editor|org.pulseaudio.pavucontrol)$"
   },
   tag = "+modal-window",
 })
--- .config/hypr/modules/envs.lua
+++ .config/hypr/modules/envs.lua
@@ -12,17 +12,19 @@ local home = os.getenv("HOME")
 
 hl.config({
 	env = {
-		"XCURSOR_SIZE,24",
-		"XCURSOR_THEME,Adwaita",
+		"CLUTTER_BACKEND,wayland",
+		"GDK_BACKEND,wayland,x11",
+		"MOZ_ENABLE_WAYLAND,1",
+		"MY_LIB_DIR," .. home .. "/.local/share/my/lib",
+		"NEWT_COLORS" .. home .. "/.config/my/current/nmtui.colors",
+		"NEWT_COLORS_FILE," .. home .. "/.config/my/current/nmtui.colors",
+		"PATH," .. home .. "/.local/share/my/bin:$PATH",
 		"QT_QPA_PLATFORM,wayland",
 		"QT_QPA_PLATFORMTHEME,qt6ct",
 		"QT_WAYLAND_DISABLE_WINDOWDECORATION,1",
 		"SDL_VIDEODRIVER,wayland",
-		"CLUTTER_BACKEND,wayland",
-		"GDK_BACKEND,wayland,x11",
-		"MOZ_ENABLE_WAYLAND,1",
-		"PATH," .. home .. "/.local/share/my/bin:$PATH",
-		"MY_LIB_DIR," .. home .. "/.local/share/my/lib",
+		"XCURSOR_SIZE,24",
+		"XCURSOR_THEME,Adwaita",
 	},
 })
 
