cdf21e089d08a7305edd5ec931193e16b3263e8b
Tyler Durham <TylerDurham@users.noreply.github.com>
2026-07-28 16:25:32 -0500

    fix(system.lua): update window rule for gnome calculator
    * dotfiles/.config/hypr/modules/applications/system.lua: include gnome calculator in the window rule to handle it as a modal utility window

--- dotfiles/.config/hypr/modules/applications/system.lua
+++ dotfiles/.config/hypr/modules/applications/system.lua
@@ -2,7 +2,7 @@
 hl.window_rule({
   name = "modal-utility-window",
   match = {
-    class = "^(.blueman-manager-wrapped|blueman-manager|nm-connection-editor|org.pulseaudio.pavucontrol)$"
+    class = "^(.blueman-manager-wrapped|blueman-manager|nm-connection-editor|org.pulseaudio.pavucontrol|org.gnome.Calculator)$"
   },
   tag = "+modal-window",
 })
