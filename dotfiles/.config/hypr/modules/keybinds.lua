9a46f9209fd2589a79f2e88b77077700e8846d7a
Tyler Durham <2191002+TylerDurham@users.noreply.github.com>
2026-08-08 22:06:25 -0500

    refactor(keybinds.lua): remove unused debug keybinding
    * dotfiles/.config/hypr/modules/keybinds.lua: remove unused debug keybinding for printing environment variable to file

--- dotfiles/.config/hypr/modules/keybinds.lua
+++ dotfiles/.config/hypr/modules/keybinds.lua
@@ -124,9 +124,3 @@ hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { description
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { description = "󰐎 Pause",      locked = true })
 hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { description = "󰒭 Next Track",  locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { description = "󰒮 Prev Track",  locked = true })
-
-----------------------------------
----- DEBUG -----------------------
-----------------------------------
-
-hl.bind("SUPER + ALT + G", hl.dsp.exec_cmd("bash -c 'printenv USER_LIB_DIR > /tmp/hypr-env-test.txt'"))
